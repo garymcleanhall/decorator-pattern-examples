3aca3a108b5a58ce33b6a1917b2826748a3bec18
Gary Hall <gary.hall@fourminutemile.co.uk>
2015-12-19 22:04:28 +0000

    Refactored all responsibilities into decorators

---
+++ DecoratorPattern/DecoratorPattern.Caching/CachingCustomerService.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+
+using DecoratorPattern.Client;
+
+namespace DecoratorPattern.Caching
+{
+    public class CachingCustomerService : ICustomerService
+    {
+        public CachingCustomerService(ICustomerService customerService)
+        {
+            if (customerService == null) throw new ArgumentNullException(nameof(customerService));
+
+            _customerService = customerService;
+            _customerNames = new Lazy<IEnumerable<string>>(GetCustomerNamesInternal);
+        }
+
+        public IEnumerable<string> GetCustomerNames()
+        {
+            return _customerNames.Value;
+        }
+
+        private IEnumerable<string> GetCustomerNamesInternal()
+        {
+            return _customerService.GetCustomerNames();
+        }
+
+        private readonly ICustomerService _customerService;
+        private readonly Lazy<IEnumerable<string>> _customerNames;
+    }
+}
--- DecoratorPattern/DecoratorPattern.Service/CustomerService.cs
+++ DecoratorPattern/DecoratorPattern.Service/CustomerService.cs
@@ -1,9 +1,7 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.Configuration;
 using System.Data;
 using System.Data.SqlClient;
-using System.Diagnostics;
 
 using DecoratorPattern.Client;
 
@@ -11,28 +9,8 @@ namespace DecoratorPattern.Service
 {
     public class CustomerService : ICustomerService
     {
-        public CustomerService(Action<string> logger)
-        {
-            if (logger == null) throw new ArgumentNullException(nameof(logger));
-
-            _logger = logger;
-            _customerNames = new Lazy<IEnumerable<string>>(GetCustomerNamesInternal);
-            _profiler = new Stopwatch();
-        }
-
         public IEnumerable<string> GetCustomerNames()
         {
-            _profiler.Restart();
-            var customerNames = _customerNames.Value;
-            _profiler.Stop();
-            _logger($"Took {_profiler.Elapsed.TotalMilliseconds}ms");
-            return customerNames;
-        } 
-
-        public IEnumerable<string> GetCustomerNamesInternal()
-        {
-            _logger("Getting customer names...");
-
             using (var connection = new SqlConnection(ConfigurationManager.ConnectionStrings["CustomerDatabase"].ConnectionString))
             {
                 connection.Open();
@@ -52,9 +30,5 @@ namespace DecoratorPattern.Service
                 connection.Close();
             }
         }
-
-        private readonly Action<string> _logger;
-        private readonly Lazy<IEnumerable<string>> _customerNames;
-        private readonly Stopwatch _profiler;
     }
 }
