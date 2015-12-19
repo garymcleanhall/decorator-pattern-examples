a7d06a6a9206a937b8ca375c8273d66264fe409d
Gary Hall <gary.hall@fourminutemile.co.uk>
2015-12-19 21:52:26 +0000

    Implemented logging, profiling and caching

--- DecoratorPattern/DecoratorPattern.Service/CustomerService.cs
+++ DecoratorPattern/DecoratorPattern.Service/CustomerService.cs
@@ -1,7 +1,9 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using System.Configuration;
 using System.Data;
 using System.Data.SqlClient;
+using System.Diagnostics;
 
 using DecoratorPattern.Client;
 
@@ -9,8 +11,28 @@ namespace DecoratorPattern.Service
 {
     public class CustomerService : ICustomerService
     {
+        public CustomerService(Action<string> logger)
+        {
+            if (logger == null) throw new ArgumentNullException(nameof(logger));
+
+            _logger = logger;
+            _customerNames = new Lazy<IEnumerable<string>>(GetCustomerNamesInternal);
+            _profiler = new Stopwatch();
+        }
+
         public IEnumerable<string> GetCustomerNames()
         {
+            _profiler.Restart();
+            var customerNames = _customerNames.Value;
+            _profiler.Stop();
+            _logger($"Took {_profiler.Elapsed.TotalMilliseconds}ms");
+            return customerNames;
+        } 
+
+        public IEnumerable<string> GetCustomerNamesInternal()
+        {
+            _logger("Getting customer names...");
+
             using (var connection = new SqlConnection(ConfigurationManager.ConnectionStrings["CustomerDatabase"].ConnectionString))
             {
                 connection.Open();
@@ -30,5 +52,9 @@ namespace DecoratorPattern.Service
                 connection.Close();
             }
         }
+
+        private readonly Action<string> _logger;
+        private readonly Lazy<IEnumerable<string>> _customerNames;
+        private readonly Stopwatch _profiler;
     }
 }
